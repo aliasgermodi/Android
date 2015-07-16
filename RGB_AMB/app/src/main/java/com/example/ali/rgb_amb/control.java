1e765a833c6b143708ba904e77b951ca8f295672
aliasgermodi <modialiasger@gmail.com>
2015-07-16 19:33:39 +0530

    addition of swtich control

--- RGB_AMB/app/src/main/java/com/example/ali/rgb_amb/control.java
+++ RGB_AMB/app/src/main/java/com/example/ali/rgb_amb/control.java
@@ -1,25 +1,23 @@
 package com.example.ali.rgb_amb;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 
-public class options extends ActionBarActivity {
+public class control extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_options);
+        setContentView(R.layout.activity_control);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_options, menu);
+        getMenuInflater().inflate(R.menu.menu_control, menu);
         return true;
     }
 
@@ -36,24 +34,5 @@ public class options extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-    public void mood(View view)
-    {
-
-        Intent i = new Intent(this,RGB.class);
-        startActivity(i);
-    }
-    public void amb(View view)
-    {
-
-        Intent k = new Intent(this,AMB.class);
-        startActivity(k);
-    }
-
-    public void cont(View view)
-    {
-
-        Intent j = new Intent(this,control.class);
-        startActivity(j);
     }
 }
